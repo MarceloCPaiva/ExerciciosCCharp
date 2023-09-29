1fc0836b06113bd1d6e8c13c9d242a41681952eb
Marcelo Paiva <marcelochagas.paiva@gmail.com>
2023-09-28 23:11:54 -0300

    Adicionando a Prova Prática 5

---
+++ ProvaPratica5/ProvaPratica5/Program.cs
@@ -0,0 +1,30 @@
+﻿// PROVA PRÁTICA 5
+
+// Escreva um programa que repita a leitura de uma senha até que ela seja válida.
+// Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida".
+// Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado.
+// Considere que a senha correta é o valor 2002.
+
+//Exemplos:
+//ENTRADA        SAÍDA
+//2200           Senha Invalida
+//1020           Senha Invalida
+//2022           Senha Invalida
+//2002           Acesso Permitido 
+
+int senha, n;
+
+senha = 2002;
+
+Console.Write("Digite a Senha: ");
+n = int.Parse(Console.ReadLine());
+
+
+while (n != senha)
+{
+    Console.WriteLine("Senha Inválida\n");
+    Console.Write("Digite a Senha: ");
+    n = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Acesso Permitido");
